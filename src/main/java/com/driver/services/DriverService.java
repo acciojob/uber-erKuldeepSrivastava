d136c8f5eada5f448c9f2727c5270992d06c97f5
erKuldeepSrivastava <96535628+erKuldeepSrivastava@users.noreply.github.com>
2023-02-26 00:42:44 +0530

    Update DriverService.java

--- src/main/java/com/driver/services/DriverService.java
+++ src/main/java/com/driver/services/DriverService.java
@@ -4,6 +4,7 @@ import com.driver.model.Driver;
 
 public interface DriverService {
 
+
 		public void register(String mobile, String password);
 		public void removeDriver(int driverId);
 		public void updateStatus(int driverId);
